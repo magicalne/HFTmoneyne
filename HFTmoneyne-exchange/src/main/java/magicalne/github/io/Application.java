47338af4af6382910d7c75a96861535cce3fa6e5
magicalne <magicalne@gmail.com>
2019-04-03 22:45:07 +0800

    test on shit coin

--- HFTmoneyne-exchange/src/main/java/magicalne/github/io/Application.java
+++ HFTmoneyne-exchange/src/main/java/magicalne/github/io/Application.java
@@ -11,15 +11,15 @@ import magicalne.github.io.wire.bitmex.OrderBookEntry;
 @Slf4j
 public class Application {
   public static void main(String[] args) throws Exception {
-    String symbol = "XBTUSD";
+    String symbol = "BCHM19";
 //    String apiKey = "b8nU8IJ6YhXdMGCws4FlpN-x";
 //    String apiSecret = "CqomC-BvhHvFbX5tX3Ztxf7tFN7ZvdELE5pqXPwqEHrXW5OM";
     String apiKey = "b8Zl6dW6qv9TqucBWw5es3B5";
     String apiSecret = "ytkJ7ii0dz0p1UAx5LM4Zr53Sg7SND1mwhk0_UW5pW-NXkNv";
     String url = "https://www.bitmex.com";
-    int scale = 10;
-    double tick = 0.5;
-    int qty = 100;
+    int scale = 10000;
+    double tick = 0.0001;
+    int qty = 1;
 
     BitmexMarket market = new BitmexMarket(symbol, apiKey, apiSecret, 5000);
     market.createWSConnection();
